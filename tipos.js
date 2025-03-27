04109af607bc9a54e48c00e98f67191ba3572606
bizk <santiago.yanzon1999@gmail.com>
2025-03-26 21:09:15 -0300

    tipos

--- tipos.js
+++ tipos.js
@@ -3,6 +3,16 @@ var nombre = "Carlos";  // Evitar usar var
 let edad = 28;
 const PI = 3.14;
 
+console.log("nombre ", nombre);
+console.log("edad ", edad);
+console.log("PI ", PI);
+
+edad = 333;
+console.log("Edad: ", edad);
+
+PI = 1
+console.log("PI: ", PI);
+
 // Tipos primitivos
 let texto = "Hola mundo";
 let numero = 42;
@@ -10,8 +20,28 @@ let esActivo = true;
 let vacio = null;
 let indefinido;
 
+if (vacio == indefinido) {
+    console.log("Son iguales");
+}
+
+if (numero > 40) {
+    console.log(numero, " es mayor a 40");
+}
+
+if (esActivo) {
+    console.log("Activo");
+}
+
 // Arrays
 let frutas = ["manzana", "banana", "uva"];
+
+for (let indice = 0; indice < frutas.length; indice++) {
+    console.log(frutas[indice]);
+}
+
+frutas.forEach(fruta => {
+    console.log(fruta);
+});
 
 // Objetos
 let persona = {
